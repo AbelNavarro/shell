6f51a46b70374c4e82a4f2dcaca0cd668e108e34
Abel Navarro <abel.navarro@gmail.com>
2017-07-06 15:23:39 +0200

    Added init-git script

---
+++ init-git/init-git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global user.name "Abel Navarro"
+git config --global user.email "abel.navarro@gmail.com"
+git config --global color.ui true
+git config --global log.decorate full
+git config --global push.default current
+git config --global core.editor vim
+
+echo "Done. Check with 'git config -l'"
+
